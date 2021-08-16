2ffd636713e910bca9acc75c23b4a77b4b9cd73a
Fernando Pioli Martínez <ferpm.dev@gmail.com>
2021-08-16 20:40:08 -0300

    ya tenemos preetier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import './styles.css';
 
 function App({ value }) {
-  const [counter, setCounter] = useState(value);
+    const [counter, setCounter] = useState(value);
 
-  const handlePlus = () => {
-    setCounter(counter + 1);
-  };
-  const handleLess = () => {
-    setCounter(counter - 1);
-  };
-  const handleReset = () => {
-    setCounter(0);
-  };
+    const handlePlus = () => {
+        setCounter(counter + 1);
+    };
+    const handleLess = () => {
+        setCounter(counter - 1);
+    };
+    const handleReset = () => {
+        setCounter(0);
+    };
 
-  return (
-    <div>
-      <h1>{counter}</h1>
-      <button onClick={handlePlus}>+</button>
-      <button onClick={handleLess}>-</button>
-      <button onClick={handleReset}>Reset</button>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+                at illum praesentium? Nam, reprehenderit. Laborum, aperiam
+                necessitatibus praesentium veritatis quibusdam voluptates
+                aliquam facere mollitia vitae in, aut commodi eaque eius.
+            </p>
+            <button onClick={handlePlus}>+</button>
+            <button onClick={handleLess}>-</button>
+            <button onClick={handleReset}>Reset</button>
+        </div>
+    );
 }
 
 export default App;
